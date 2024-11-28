b710a34a98e555d675eeb34a33bc76ae8570acb0
Hounaida202 <etrichih@gmail.com>
2024-11-28 15:21:04 +0100

    Ajouter la structure et le UI de la section de la banque de remplacement

---
+++ index/script.js
@@ -0,0 +1,23 @@
+// let lw=document.getElementById("lw");
+// let st=document.getElementById("st");
+// let rw=document.getElementById("rw");
+// let cm1=document.getElementById("cm1");
+// let cm2=document.getElementById("cm2");
+// let cm3=document.getElementById("cm3");
+// let lb=document.getElementById("lb");
+// let cb1=document.getElementById("cb1");
+// let cb2=document.getElementById("cb2");
+// let rb=document.getElementById("rb");
+// let gk=document.getElementById("gk");
+
+// let btnn = document.getElementById("btnn");
+// btnn.addEventListener("click", function() {
+//     fonction(data);
+// });
+        
+
+
+
+
+
+
