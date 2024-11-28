c722e60ef27f843fae6e1cd88a7406b8ad45ffd1
Hounaida202 <etrichih@gmail.com>
2024-11-28 15:25:05 +0100

    Ajouter la fonction (remplirCartes) pour remplir les cartes depuis les data récupéré , avec une boucle

--- index/script.js
+++ index/script.js
@@ -34,5 +34,48 @@ return response.json();
 });
 
 
+function fonction(objet) {
+    const players = objet.players;
+    let cardes="";
+
+    for(let i=0;i<players.length; i++){
+cardes+=`
+<div class="carte">
+        <div class="player-card-js">
+    <div class="rating">${players[i].rating}</div>
+    <div class="image-div"><img  src="${players[i].photo}" alt=""></div>
+    <div class="name">${players[i].name}</div>
+      <div class="club-info">
+           <div class="flag-div">
+          <div class="nationality">${players[i].nationality}</div>
+          <img src="${players[i].flag}" alt="">
+           </div>
+           <div class="club-div">
+           <div class="club">${players[i].club}</div>
+           <img src="${players[i].logo}" alt="">
+           </div>
+      </div>
+      <div class="score">
+           <div class="score-1">
+               <div class="pas" > Pas:${players[i].passing}</div>
+                <div class="shot" >Shoot:${players[i].shooting}</div>
+                <div class="pace" >Pace:${players[i].pace}</div>
+           </div>
+           
+           <div class="score-2">
+               <div class="drib" >Drib:${players[i].dribbling}</div>
+               <div class="phys" >Phys:${players[i].physical}</div>
+               <div class="def" >Def:${players[i].defending}</div>
+           </div>
+      </div>
+  </div>
+</div> 
+`
+}
+document.querySelector(".auto-remplissage-div").innerHTML = cardes;
+
+};
+
+
 
 
