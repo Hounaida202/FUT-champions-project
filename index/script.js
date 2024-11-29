0a3c0667a5bf8d28e71b972ddf7dc73ae5610c15
Hounaida202 <etrichih@gmail.com>
2024-11-29 10:02:47 +0100

    ajouter une fonction qui a accès au HTML et qui ajoute les cartes dans l'affichage dans la section des cartes

--- index/script.js
+++ index/script.js
@@ -88,6 +88,47 @@ function afficherJoueursFiltré(parametrepPourPlayer,parametrePourPosition){
     let joueursfiltré=parametrepPourPlayer.filter(joueur=>joueur.position===parametrePourPosition)
      FonctionAfficher(joueursfiltré);
     }
+    function FonctionAfficher(joueursfiltré){
+        let carte="";
+        let container = document.querySelector(".auto-remplissage-div");
+        for(let i=0 ; i < joueursfiltré.length ; i++){
+        carte+=`
+        <div class="carte">
+                <div class="player-card-js">
+            <div class="rating">${joueursfiltré[i].rating}</div>
+            <div class="image-div"><img  src="${joueursfiltré[i].photo}" alt=""></div>
+            <div class="name">${joueursfiltré[i].name}</div>
+              <div class="club-info">
+                   <div class="flag-div">
+                  <div class="nationality">${joueursfiltré[i].nationality}</div>
+                  <img src="${joueursfiltré[i].flag}" alt="">
+                   </div>
+                   <div class="club-div">
+                   <div class="club">${joueursfiltré[i].club}</div>
+                   <img src="${joueursfiltré[i].logo}" alt="">
+                   </div>
+              </div>
+              <div class="score">
+                   <div class="score-1">
+                       <div class="pas" > Pas:${joueursfiltré[i].passing}</div>
+                        <div class="shot" >Shoot:${joueursfiltré[i].shooting}</div>
+                        <div class="pace" >Pace:${joueursfiltré[i].pace}</div>
+                   </div>
+                   
+                   <div class="score-2">
+                       <div class="drib" >Drib:${joueursfiltré[i].dribbling}</div>
+                       <div class="phys" >Phys:${joueursfiltré[i].physical}</div>
+                       <div class="def" >Def:${joueursfiltré[i].defending}</div>
+                   </div>
+              </div>
+          </div>
+        </div> 
+        `
+        container.innerHTML=carte;
+        
+                }
+        
+            }
 
 // les fonction des quitter et entrer
 let exitAll=document.getElementById("exit-all");
